4ea84895459ad92dacfd3c763548f5bbbaf9ba73
Fotoz <fotozxx@gmail.com>
2018-02-17 14:18:56 +0200

    footer

--- js/common.js
+++ js/common.js
@@ -3,27 +3,18 @@ $(function() {
 // slick
 	$(window).on("load resize", function() {
 		var width = $(document).width();
-	  
+
 		if (width > 768) {
 			$('.slider').slick('unslick');
-	  	} else {
-	    	$('.slider').not('.slick-initialized').slick({  
-	    		slidesToShow: 1,
-	      		slidesToScroll: 1,
-	      		infinite: true,
-	      		dots: true,
-	      		adaptiveHeight: true
-	    	});
-	  	};
+		} else {
+			$('.slider').not('.slick-initialized').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true,
+				adaptiveHeight: true
+			});
+		};
 	});
-
-// smooth-scroling
-    var $page = $('html, body');
-        $('a[href*="#"]').click(function() {
-            $page.animate({
-                scrollTop: $($.attr(this, 'href')).offset().top
-            }, 800);
-            return false;
-    });
 
 });
